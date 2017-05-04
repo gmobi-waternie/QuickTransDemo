280633b15ba9260068e08189350d444f520c502f
waternie <waternie@hotmail.com>
2017-05-04 16:23:34 +0800

    init

--- app/src/main/java/com/gmobi/quicktrans/activity/MainActivity.java
+++ app/src/main/java/com/gmobi/quicktrans/activity/MainActivity.java
@@ -413,12 +413,7 @@ public class MainActivity extends Activity implements EasyPermissions.Permission
         return (networkInfo != null && networkInfo.isConnected());
     }
 
-    /**
-     * Check that Google Play services APK is installed and up to date.
-     *
-     * @return true if Google Play Services is available and up to
-     * date on this device; false otherwise.
-     */
+
     private boolean isGooglePlayServicesAvailable() {
         GoogleApiAvailability apiAvailability =
                 GoogleApiAvailability.getInstance();
@@ -427,10 +422,7 @@ public class MainActivity extends Activity implements EasyPermissions.Permission
         return connectionStatusCode == ConnectionResult.SUCCESS;
     }
 
-    /**
-     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
-     * Play Services installation via a user dialog, if possible.
-     */
+
     private void acquireGooglePlayServices() {
         GoogleApiAvailability apiAvailability =
                 GoogleApiAvailability.getInstance();
@@ -442,13 +434,7 @@ public class MainActivity extends Activity implements EasyPermissions.Permission
     }
 
 
-    /**
-     * Display an error dialog showing that Google Play Services is missing
-     * or out of date.
-     *
-     * @param connectionStatusCode code describing the presence (or lack of)
-     *                             Google Play Services on this device.
-     */
+
     void showGooglePlayServicesAvailabilityErrorDialog(
             final int connectionStatusCode) {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
